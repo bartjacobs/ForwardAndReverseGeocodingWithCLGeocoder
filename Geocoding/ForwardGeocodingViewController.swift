f51e6b621094c52d4e62c12f87857786c7d37eb0
Bart Jacobs <developer@bartjacobs.com>
2016-09-25 12:51:37 +0200

    Create user interface ForwardGeocodingViewController

--- Geocoding/ForwardGeocodingViewController.swift
+++ Geocoding/ForwardGeocodingViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class ForwardGeocodingViewController: UIViewController {
 
+    // MARK: - Properties
+
+    @IBOutlet var countryTextField: UITextField!
+    @IBOutlet var streetTextField: UITextField!
+    @IBOutlet var cityTextField: UITextField!
+
+    @IBOutlet var geocodeButton: UIButton!
+    @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+
+    @IBOutlet var locationLabel: UILabel!
+
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    // MARK: - Actions
+
+    @IBAction func geocode(_ sender: UIButton) {
+        
     }
 
 }
