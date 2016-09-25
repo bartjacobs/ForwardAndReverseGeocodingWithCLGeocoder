9d8125a7aace5132f787381faf5c8b7b755e55cb
Bart Jacobs <developer@bartjacobs.com>
2016-09-25 13:14:49 +0200

    Add ability to forward geocode address

--- Geocoding/ForwardGeocodingViewController.swift
+++ Geocoding/ForwardGeocodingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ForwardGeocodingViewController: UIViewController {
 
@@ -21,6 +22,10 @@ class ForwardGeocodingViewController: UIViewController {
 
     @IBOutlet var locationLabel: UILabel!
 
+    // MARK: -
+
+    lazy var geocoder = CLGeocoder()
+
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
@@ -30,7 +35,49 @@ class ForwardGeocodingViewController: UIViewController {
     // MARK: - Actions
 
     @IBAction func geocode(_ sender: UIButton) {
-        
+        guard let country = countryTextField.text else { return }
+        guard let street = streetTextField.text else { return }
+        guard let city = cityTextField.text else { return }
+
+        // Create Address String
+        let address = "\(country), \(city), \(street)"
+
+        // Geocode Address String
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            // Process Response
+            self.processResponse(withPlacemarks: placemarks, error: error)
+        }
+
+        // Update View
+        geocodeButton.isHidden = true
+        activityIndicatorView.startAnimating()
+    }
+
+    // MARK: - Helper Methods
+
+    private func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
+        // Update View
+        geocodeButton.isHidden = false
+        activityIndicatorView.stopAnimating()
+
+        if let error = error {
+            print("Unable to Forward Geocode Address (\(error))")
+            locationLabel.text = "Unable to Find Location for Address"
+
+        } else {
+            var location: CLLocation?
+            
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+
+            if let location = location {
+                let coordinate = location.coordinate
+                locationLabel.text = "\(coordinate.latitude), \(coordinate.longitude)"
+            } else {
+                locationLabel.text = "No Matching Location Found"
+            }
+        }
     }
 
 }
